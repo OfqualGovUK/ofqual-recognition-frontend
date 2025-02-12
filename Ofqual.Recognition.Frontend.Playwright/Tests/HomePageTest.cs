9b2ed622bbd830b87b1c488f7b2a33dac6cd0f24
harshapreddy <harshareddy.leeds@gmail.com>
2025-02-12 09:11:22 +0000

    57702 - Added a sample homepage test

---
+++ Ofqual.Recognition.Frontend.Playwright/Tests/HomePageTest.cs
@@ -0,0 +1,20 @@
+﻿
+using Ofqual.Recognition.Frontend.Playwright.Pages;
+
+namespace Ofqual.Recognition.Frontend.Playwright.Tests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class launchHomePage : PageTest
+{
+
+    [Test]
+    public async Task LaunchHomePage()
+    {
+        var homePage = new HomePage(Page);
+
+        await homePage.GoToHomePage(); ;
+        await homePage.checkPageHeading("Apply to have your qualifications recognised");
+    }
+
+}
