b3648f500068c5319f31869ea95958075e8d2867
Robert Mason <robert.mason@ofqual.gov.uk>
2025-02-13 10:57:03 +0000

    Axe creates reports in a reports folder in the bin Debug working directory

--- Ofqual.Recognition.Frontend.Playwright/Tests/HomePageTest.cs
+++ Ofqual.Recognition.Frontend.Playwright/Tests/HomePageTest.cs
@@ -1,5 +1,6 @@
 ﻿
 using Ofqual.Recognition.Frontend.Playwright.Pages;
+using Playwright.Axe;
 
 namespace Ofqual.Recognition.Frontend.Playwright.Tests;
 
@@ -15,6 +16,11 @@ public class launchHomePage : PageTest
 
         await homePage.GoToHomePage(); ;
         await homePage.checkPageHeading("Apply to have your qualifications recognised");
+
+        //Run an axe accessibility scan on the page
+        AxeHtmlReportOptions reportOptions = new(reportDir: ".//reports");
+        AxeResults axeResults = await Page.RunAxe(reportOptions: reportOptions);
+        Console.WriteLine($"Axe ran against {axeResults.Url} on {axeResults.Timestamp}.");
     }
 
 }
