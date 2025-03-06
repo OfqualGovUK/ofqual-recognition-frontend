29a255693d07fd709b341f18ea7e913dce88cd86
Sommos <Sommosgaming@gmail.com>
2025-03-06 11:35:40 +0000

    [58087] WIP: added frontend pages and basic program flow controller

---
+++ Ofqual.Recognition.Frontend.Web/Controllers/EligibilityController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ofqual.Recognition.Frontend.Web.Controllers
+{
+    public class EligibilityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult QuestionOne()
+        {
+            return View();
+        }
+
+        public IActionResult QuestionTwo()
+        {
+            return View();
+        }
+
+        public IActionResult QuestionThree()
+        {
+            return View();
+        }
+
+        public IActionResult Eligible()
+        {
+            return View();
+        }
+
+        public IActionResult NotEligible()
+        {
+            return View();
+        }
+    }
+}
