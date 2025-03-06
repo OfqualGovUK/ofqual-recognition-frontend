6ce615736537142469921f9242428852c7cd13fd
Sommos <Sommosgaming@gmail.com>
2025-03-06 14:25:41 +0000

    [58087] ADD: QuestionCheck summary card view & basic error handling

--- Ofqual.Recognition.Frontend.Web/Models/EligibilityModel.cs
+++ Ofqual.Recognition.Frontend.Web/Models/EligibilityModel.cs
@@ -1,11 +1,16 @@
-﻿namespace Ofqual.Recognition.Frontend.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ofqual.Recognition.Frontend.Web.Models
 {
-    public class Eligibility
+    public class EligibilityModel
     {
+        [Required(ErrorMessage = "Please select an option for Question One.")]
         public string QuestionOne { get; set; }
+        [Required(ErrorMessage = "Please select an option for Question Two.")]
         public string QuestionTwo { get; set; }
+        [Required(ErrorMessage = "Please select an option for Question Three.")]
         public string QuestionThree { get; set; }
 
-        public bool IsEligible() => QuestionOne == "yes" && QuestionTwo == "yes" && QuestionThree == "yes";
+        public bool IsEligible() => QuestionOne == "Yes" && QuestionTwo == "Yes" && QuestionThree == "Yes";
     }
 }
