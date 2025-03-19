5005d6cae82743ca79f59be63f449e29711c1254
Sommos <Sommosgaming@gmail.com>
2025-03-19 12:45:35 +0000

    [58095] ADD: basic UI task list skeleton

---
+++ Ofqual.Recognition.Frontend.Web/Controllers/ApplicationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ofqual.Recognition.Frontend.Web.Controllers
+{
+    [Route("application")]
+    public class ApplicationController : Controller
+    {
+        private readonly ILogger<ApplicationController> _logger;
+
+        public ApplicationController(ILogger<ApplicationController> logger)
+        { 
+            _logger = logger;
+        }
+
+        [HttpGet("tasks")]
+        public IActionResult TaskList()
+        {
+            // attempt to get an applicationId via creation or cache
+
+            // get task list with statuses
+
+            // display tasks in their sections
+            return View();
+        }
+
+        [HttpGet("check-your-answers")]
+        public IActionResult TaskCheck() {
+            // add ?taskId={taskId} to url check-your-answers?taskId={taskId}
+            return View();
+        }
+    }
+}
