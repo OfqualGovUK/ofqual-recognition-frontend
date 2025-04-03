031ac7c6bb130c504ed1e0eaa3d4173a034ce23b
Sommos <Sommosgaming@gmail.com>
2025-04-03 10:26:44 +0100

    [58387] ADD: FirstQuestionURL added to model for redirection

--- Ofqual.Recognition.Frontend.Web/ViewModels/TaskList/TaskItemViewModel.cs
+++ Ofqual.Recognition.Frontend.Web/ViewModels/TaskList/TaskItemViewModel.cs
@@ -8,11 +8,12 @@ public class TaskItemViewModel
     public Guid TaskId { get; set; }
     public string TaskName { get; set; } = string.Empty;
     public TaskStatusEnum Status { get; set; }
+    public required String FirstQuestionURL { get; set; }
 
     public bool IsLink => Status != TaskStatusEnum.CannotStartYet;
-    public string Url => TaskName == "Review application"
-        ? $"/application/review-your-application-answers?taskId={TaskId}"
-        : $"/application/review-your-task-answers?taskId={TaskId}";
+    //public string Url => TaskName == "Review application"
+    //    ? $"/application/review-your-application-answers?taskId={TaskId}"
+    //    : $"/application/review-your-task-answers?taskId={TaskId}";
     public string StatusDisplay => Status.GetDisplayName();
     public string TagClass => Status.GetTagClass();
 }
