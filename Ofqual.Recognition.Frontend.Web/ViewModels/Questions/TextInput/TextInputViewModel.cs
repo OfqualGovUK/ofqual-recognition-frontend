fd2d6ff5641fc56b83badc047d78ffc07e8d7129
RichBOfqual <Rich.Batten@ofqual.gov.uk>
2025-04-17 12:16:12 +0100

    [58166] MODIFY: Renamed Branch. Updated model to have required text inputs. Improved spacing in partial view.

--- Ofqual.Recognition.Frontend.Web/ViewModels/Questions/TextInput/TextInputViewModel.cs
+++ Ofqual.Recognition.Frontend.Web/ViewModels/Questions/TextInput/TextInputViewModel.cs
@@ -10,7 +10,7 @@ public class TextInputViewModel
     /// <summary>
     /// The collection of text input fields in the group.
     /// </summary>
-    public List<TextInputItemViewModel> TextInputs { get; set; }
+    public required List<TextInputItemViewModel> TextInputs { get; set; }
 
     /// <summary>
     /// The display name for the section shown on the review page.
