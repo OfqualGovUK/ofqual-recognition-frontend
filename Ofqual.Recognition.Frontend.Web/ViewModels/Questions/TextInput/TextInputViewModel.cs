b60928854e053fe4282f83cf92441b5ff02ea582
RichBOfqual <Rich.Batten@ofqual.gov.uk>
2025-04-17 12:10:43 +0100

    [58166] MODIFY: Updated models and partial view

--- Ofqual.Recognition.Frontend.Web/ViewModels/Questions/TextInput/TextInputViewModel.cs
+++ Ofqual.Recognition.Frontend.Web/ViewModels/Questions/TextInput/TextInputViewModel.cs
@@ -3,6 +3,11 @@ namespace Ofqual.Recognition.Frontend.Web.ViewModels;
 public class TextInputViewModel
 {
     /// <summary>
+    /// The heading shown above the text inputs.
+    /// </summary>
+    public TextWithSizeViewModel? Heading { get; set; }
+
+    /// <summary>
     /// The collection of text input fields in the group.
     /// </summary>
     public List<TextInputItemViewModel> TextInputs { get; set; }
