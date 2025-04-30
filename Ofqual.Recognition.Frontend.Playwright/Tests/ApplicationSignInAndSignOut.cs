41dfb49ed0d97b8e219d39ecf687cdb38f057fee
Robert Mason <robert.mason@ofqual.gov.uk>
2025-04-30 16:44:07 +0100

    basic passing sign in and sign out test in test automation mode, which will need refactoring to make the password secret, and we will want to split sign in and sign out tests at some point.

---
+++ Ofqual.Recognition.Frontend.Playwright/Tests/ApplicationSignInAndSignOut.cs
@@ -0,0 +1,20 @@
+﻿using Ofqual.Recognition.Frontend.Playwright.Pages;
+namespace Ofqual.Recognition.Frontend.Playwright.Tests;
+[TestFixture]
+public class ApplicationSignInAndSignOut : PageTest
+{
+    [Test]
+    public async Task SignInAndSignOut()
+    {
+        var homePage = new HomePage(Page);
+
+        //sign in
+
+        await homePage.GoToHomePage();
+
+        await homePage.CheckSignInAndSignOut();
+
+      
+    }
+ 
+}
