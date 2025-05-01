59d8cd0bc0823fe830a26e3351f87fb807ac133a
Luke Hudson <Luke.Hudson@ofqual.gov.uk>
2025-05-01 09:35:03 +0100

    [58482] ADD: Playwright screenshot functionality

--- Ofqual.Recognition.Frontend.Playwright/Tests/ApplicationSignInAndSignOut.cs
+++ Ofqual.Recognition.Frontend.Playwright/Tests/ApplicationSignInAndSignOut.cs
@@ -1,8 +1,23 @@
-﻿using Ofqual.Recognition.Frontend.Playwright.Pages;
+﻿using NUnit.Framework.Interfaces;
+using Ofqual.Recognition.Frontend.Playwright.Pages;
+using System.Globalization;
 namespace Ofqual.Recognition.Frontend.Playwright.Tests;
 [TestFixture]
 public class ApplicationSignInAndSignOut : PageTest
 {
+    [TearDown]
+    public async Task TakeScreenshotOnFailure()
+    {
+        var context = TestContext.CurrentContext;
+
+        if (context.Result.Outcome.Equals(ResultState.Error))
+            await Page.ScreenshotAsync(new()
+            {
+                FullPage = true,
+                Path = $"Failure__{context.Test.FullName}_{DateTime.Now.ToString("o", CultureInfo.InvariantCulture)}.png"
+            });
+    }
+
     [Test]
     public async Task SignInAndSignOut()
     {
--- Ofqual.Recognition.Frontend.Playwright/Tests/EligibilityQuestionsTest.cs
+++ Ofqual.Recognition.Frontend.Playwright/Tests/EligibilityQuestionsTest.cs
@@ -1,5 +1,7 @@
-﻿using Ofqual.Recognition.Frontend.Playwright.Pages;
+﻿using NUnit.Framework.Interfaces;
+using Ofqual.Recognition.Frontend.Playwright.Pages;
 using Playwright.Axe;
+using System.Globalization;
 
 namespace Ofqual.Recognition.Frontend.Playwright.Tests;
 
@@ -7,6 +9,20 @@ namespace Ofqual.Recognition.Frontend.Playwright.Tests;
 [TestFixture]
 public class EligibilityQuestionsTest : PageTest
 {
+
+    [TearDown]
+    public async Task TakeScreenshotOnFailure()
+    {
+        var context = TestContext.CurrentContext;
+
+        if (context.Result.Outcome.Equals(ResultState.Error))       
+            await Page.ScreenshotAsync(new()
+            {
+                FullPage = true,
+                Path = $"Failure__{context.Test.FullName}_{DateTime.Now.ToString("o", CultureInfo.InvariantCulture)}.png"
+            });      
+    }
+
     [Test]
     public async Task EligibleDecision()
     {
