04f480e5dea78ac4f535bf77d3ad66832a83c404
alcharra <168999056+alcharra@users.noreply.github.com>
2025-06-12 17:14:11 +0100

    [58741] MODIFY: update the validation rule view model to exclude CountCharacters

--- Ofqual.Recognition.Frontend.Web/ViewModels/Questions/Shared/ValidationRuleViewModel.cs
+++ Ofqual.Recognition.Frontend.Web/ViewModels/Questions/Shared/ValidationRuleViewModel.cs
@@ -21,20 +21,12 @@ public class ValidationRuleViewModel
     public bool? CountWords { get; set; }
 
     /// <summary>
-    /// Validates MinLength and MaxLength by counting the number of characters.
-    /// Ignored for non-text input components.
-    /// </summary>
-    public bool? CountCharacters { get; set; }
-
-    /// <summary>
-    /// Requires a minimum number of characters or words (depending on CountWords / CountCharacters).
-    /// Ignored for non-text input components.
+    /// Requires a minimum number of characters or words (words only if CountWords is true).
     /// </summary>
     public int? MinLength { get; set; }
 
     /// <summary>
-    /// Requires a maximum number of characters or words (depending on CountWords / CountCharacters).
-    /// Ignored for non-text input components.
+    /// Requires a maximum number of characters or words (words only if CountWords is true).
     /// </summary>
     public int? MaxLength { get; set; }
 
@@ -49,7 +41,7 @@ public class ValidationRuleViewModel
     /// Ignored for text input components.
     /// </summary>
     public int? MaxSelected { get; set; }
-
+    
     /// <summary>
     /// Regex pattern to validate input format.
     /// Ignored for non-text input components.
