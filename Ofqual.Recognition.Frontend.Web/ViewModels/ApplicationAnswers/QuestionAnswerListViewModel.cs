227b9666ac77557ba1d90388eea04bd1469a4b37
RichBOfqual <Rich.Batten@ofqual.gov.uk>
2025-06-23 16:24:36 +0100

    [58168] MODIFY: Changed the branch name to what it should be. Moved files into the correct locations

---
+++ Ofqual.Recognition.Frontend.Web/ViewModels/ApplicationAnswers/QuestionAnswerListViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Ofqual.Recognition.Frontend.Web.ViewModels;
+
+public class QuestionAnswerListViewModel : TaskListViewModel
+{
+    public bool IsReadOnly { get; set; } = false;
+
+    // List of questions and their metadata
+    public List<QuestionAnswerItemViewModel> Items { get; set; } = new();
+}
+
+public class QuestionAnswerItemViewModel
+{
+    public string Question { get; set; }
+    public object Answer { get; set; }
+    public string ChangeUrl { get; set; }
+}
