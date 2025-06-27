fce79a7077879c514b2bb8189d611d9d00327a00
Luke Hudson <Luke.Hudson@ofqual.gov.uk>
2025-06-27 15:36:58 +0100

    [58170] MODIFY: correct submission logic so that the correct page is returned

--- Ofqual.Recognition.Frontend.Infrastructure/Services/Interfaces/IApplicationService.cs
+++ Ofqual.Recognition.Frontend.Infrastructure/Services/Interfaces/IApplicationService.cs
@@ -5,4 +5,6 @@ namespace Ofqual.Recognition.Frontend.Infrastructure.Services.Interfaces;
 public interface IApplicationService
 {
     public Task<Application?> SetUpApplication();
+
+    public Task<bool> CompleteApplication(Guid applicationId);
 }
