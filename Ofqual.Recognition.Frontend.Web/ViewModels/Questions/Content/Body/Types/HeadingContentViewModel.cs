1733cb67871acf1cc7e917d9fad00a8813cfb374
alcharra <168999056+alcharra@users.noreply.github.com>
2026-01-29 14:54:47 +0000

    [61120] MODIFY: include heading level to control header size

--- Ofqual.Recognition.Frontend.Web/ViewModels/Questions/Content/Body/Types/HeadingContentViewModel.cs
+++ Ofqual.Recognition.Frontend.Web/ViewModels/Questions/Content/Body/Types/HeadingContentViewModel.cs
@@ -7,4 +7,5 @@ public class HeadingContentViewModel
 {
     public required string Text { get; set; }
     public BodyTextSize Size { get; set; } = BodyTextSize.L;
+    public int HeadingLevel { get; set; } = 1;
 }
